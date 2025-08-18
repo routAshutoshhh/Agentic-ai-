31380a3fb0122eb28dd0f80c08780268d8becf85
routAshutoshhh <egoisticdev@gmail.com>
2025-08-18 15:38:12 +0530

    starting the project for the buildibng api server

--- langchain-learning/building-apis-langserve/main.py
+++ langchain-learning/building-apis-langserve/main.py
@@ -3,3 +3,38 @@ from langserve  import add_routes
 from langchain.prompts import ChatPromptTemplate
 from langchain_ollama import OllamaLLM
 
+import uvicorn
+import os
+from dotenv import load_dotenv
+
+
+#loading the dotenv
+load_dotenv()
+
+
+
+##creating a fastapi  application 
+app = FastAPI(
+    title="LangChain API",
+    description="API  server for interacting with LangChain",
+    version="0.1.0"
+)
+
+
+##creating routes using langserve
+add_routes(
+    app,
+    OllamaLLM(),
+    path="/api/ollama"
+)
+
+#creating models 
+#creating open ai llm model - that we are not going to use for the things
+openai_llm =
+
+
+#creating a  llama model 
+llama_llm = OllamaLLM(model="phi:latest")
+
+#creating prompt
+prompt = ChatPromptTemplate.from_template("write me an essay for coding beneifts ")
